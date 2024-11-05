92bae607e2c61e484a8c81a77d0070a693333eb3
Victor R <39545521+viicslen@users.noreply.github.com>
2024-11-05 16:50:15 -0500

    Add hashed password for Sheikh user configuration

--- users/sheikh.nix
+++ users/sheikh.nix
@@ -8,6 +8,8 @@ in {
     isNormalUser = true;
     extraGroups = [user "www-data" "networkmanager"];
 
+    hashedPassword = "$y$j9T$BwWRM82WwZ0R5oYq7g5rc1$BWX3/Nyd0H8ViJ8hxPvd3vzOcrNgRWOv/JTRt4zRU28";
+
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAr1HrvgiEIaJ2ZofvdzoQs6PuwOpaEeVdirt80fAtL0 ${user}@remote"
     ];
