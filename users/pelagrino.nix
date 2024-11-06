4f65ae0cc705ec7c0794cd9a7078039c023fca68
Victor R <39545521+viicslen@users.noreply.github.com>
2024-11-06 12:09:18 -0500

    Remove unnecessary tmux initialization from pelagrino.nix configuration

--- users/pelagrino.nix
+++ users/pelagrino.nix
@@ -95,12 +95,6 @@ in {
             file = "fzf-tab.plugin.zsh";
           }
         ];
-
-        initExtra = ''
-          if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]]; then
-            tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
-          fi
-        '';
       };
     };
 
