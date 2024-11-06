32e23947f2c7b3811862db0ada6865e2160d1199
Victor R <39545521+viicslen@users.noreply.github.com>
2024-11-06 12:18:47 -0500

    Enable home-manager default programs and configurations for SSH, git, fzf, zoxide, hstr, and zsh with plugins

--- modules/home-manager/defaults/default.nix
+++ modules/home-manager/defaults/default.nix
@@ -1,5 +1,6 @@
 {
   stateVersion,
+  inputs,
   lib,
   ...
 }: {
@@ -21,6 +22,83 @@
         COREPACK_ENABLE_AUTO_PIN = "0";
         COREPACK_ENABLE_PROJECT_SPEC = "0";
       };
+    };
+
+    # Enable programs
+    programs = {
+      # Allow home-manager to manage itself
+      home-manager.enable = true;
+
+      # Enable SSH
+      ssh.enable = true;
+
+      # Enable git and delta
+      git = {
+        enable = true;
+        delta.enable = true;
+      };
+
+      # Enable fzf
+      fzf = {
+        enable = true;
+        enableZshIntegration = true;
+        tmux.enableShellIntegration = true;
+      };
+
+      # Enable zoxide
+      zoxide = {
+        enable = true;
+        enableZshIntegration = true;
+        options = ["--cmd cd"];
+      };
+
+      # Enable hstr
+      hstr = {
+        enable = true;
+        enableZshIntegration = true;
+      };
+
+      # Enable zsh with plugins
+      zsh = {
+        enable = true;
+        syntaxHighlighting.enable = true;
+        autosuggestion.enable = true;
+
+        shellAliases = {
+          ls = "lsd";
+          l = "ls -l";
+          la = "ls -a";
+          lla = "ls -la";
+          lt = "ls --tree";
+          pn = "pnpm";
+          cat = "bat";
+        };
+
+        oh-my-zsh = {
+          enable = true;
+          plugins = [
+            "git"
+            "npm"
+            "node"
+            "zoxide"
+            "zsh-interactive-cd"
+          ];
+        };
+
+        plugins = [
+          {
+            name = "fzf-tab";
+            src = inputs.fzf-tab;
+            file = "fzf-tab.plugin.zsh";
+          }
+        ];
+      };
+    };
+
+    # Enable tmux
+    presets.tmux = {
+      enable = true;
+      zshIntegration = true;
     };
   };
 }
--- users/sheikh.nix
+++ users/sheikh.nix
@@ -16,93 +16,9 @@ in {
   };
 
   home-manager.users.${user} = {
-    # Configure the home environment
     home = {
       username = user;
       homeDirectory = "/home/${user}";
-    };
-
-    # Enable programs
-    programs = {
-      # Allow home-manager to manage itself
-      home-manager.enable = true;
-
-      # Enable SSH
-      ssh.enable = true;
-
-      # Enable git and delta
-      git = {
-        enable = true;
-        delta.enable = true;
-      };
-
-      # Enable fzf
-      fzf = {
-        enable = true;
-        enableZshIntegration = true;
-        tmux.enableShellIntegration = true;
-      };
-
-      # Enable zoxide
-      zoxide = {
-        enable = true;
-        enableZshIntegration = true;
-        options = ["--cmd cd"];
-      };
-
-      # Enable hstr
-      hstr = {
-        enable = true;
-        enableZshIntegration = true;
-      };
-
-      # Enable zsh with plugins
-      zsh = {
-        enable = true;
-        syntaxHighlighting.enable = true;
-
-        autosuggestion = {
-          enable = true;
-          # highlight = "fg=#ff00ff,bg=cyan,bold,underline";
-          # strategy = ["history" "completion"];
-        };
-
-        shellAliases = {
-          ls = "lsd";
-          l = "ls -l";
-          la = "ls -a";
-          lla = "ls -la";
-          lt = "ls --tree";
-          pn = "pnpm";
-          cat = "bat";
-        };
-
-        oh-my-zsh = {
-          enable = true;
-          plugins = [
-            "git"
-            "npm"
-            "node"
-            "docker"
-            "zoxide"
-            "zsh-interactive-cd"
-          ];
-        };
-
-        plugins = [
-          {
-            name = "fzf-tab";
-            src = inputs.fzf-tab;
-            file = "fzf-tab.plugin.zsh";
-          }
-        ];
-      };
-    };
-
-    # Enable tmux
-    presets.tmux = {
-      enable = true;
-      zshIntegration = true;
     };
   };
 }
