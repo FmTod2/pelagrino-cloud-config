f42e003a763095ebb0cc71cbc0d330ec3367773e
Victor R <39545521+viicslen@users.noreply.github.com>
2024-11-06 10:59:45 -0500

    Configure session variables and state version in home-manager defaults

--- modules/home-manager/defaults/default.nix
+++ modules/home-manager/defaults/default.nix
@@ -4,14 +4,23 @@
   ...
 }: {
   config = {
-    # Set state version
-    home.stateVersion = lib.mkDefault stateVersion;
-
-    # Configure the package manager
-    nixpkgs.config = import ./nixpkgs.nix;
-    xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs.nix;
-
     # Use sd-switch to manage systemd services
     systemd.user.startServices = lib.mkDefault "sd-switch";
+
+    # Configure the package manager
+    xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs.nix;
+    nixpkgs.config = import ./nixpkgs.nix;
+
+    home = {
+      # Set state version
+      stateVersion = lib.mkDefault stateVersion;
+
+      # Set session variables
+      sessionVariables = {
+        COREPACK_ENABLE_STRICT = 0;
+        COREPACK_ENABLE_AUTO_PIN = 0;
+        COREPACK_ENABLE_PROJECT_SPEC = 0;
+      };
+    };
   };
 }
